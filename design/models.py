edace5aebad3c344fa3a2c67f3d1352acd714af8
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-20 21:40:42 +0200

    database changed

--- design/models.py
+++ design/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Photos(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+
+    GENDER_CHOICES = [
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    ]
+    image=models.ImageField()
+    title=models.CharField(max_length=100)
+    gender = models.CharField(max_length=5, choices=GENDER_CHOICES)
+    price=models.CharField(max_length=250)
+    Sales=models.BooleanField(default=False)
--- design/views.py
+++ design/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import *
 # Create your views here.
 def shop(request):
- return render(request, 'shop.html')
+ selectData=Photos.objects.all()
+ return render(request,"shop.html",{'data':selectData})
