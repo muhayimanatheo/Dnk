60272ef98d1ebff147d75275cd3b928a99d4be96
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-22 01:29:30 +0200

    yes changes

--- design/models.py
+++ design/models.py
@@ -1,9 +1,23 @@
-from django.db import models
+from django.db import models    
+from django.utils import timezone
 
 # Create your models here.
 class Photos(models.Model):
     image=models.ImageField()
     title=models.CharField(max_length=100)
-    gender = models.CharField(max_length=100)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='O')
     price=models.CharField(max_length=250)
     Sales=models.BooleanField(default=False)
+    
+
+
+
+
+    
+
+   
